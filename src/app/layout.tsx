4548e8bd86032c574476dca60a3539fcce3de967
ShaunT-bit <tshaun114@gmail.com>
2025-02-15 16:25:42 +0400

    New commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,24 +2,27 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Syntheous",
-  description: "A f1 group",
+    title: "Syntheous",
+    description: "A F1 group",
 };
 
 export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
+                                       children,
+                                   }: Readonly<{
+    children: React.ReactNode;
 }>) {
-  return (
-    <html className="scroll-smooth" lang="en">
-      <body className={(inter.className,"bg-black text-[#F5FFFA] antialiased")}>
-      {children}
-      </body>
-
-    </html>
-  );
+    return (
+        <html className="scroll-smooth" lang="en">
+        <head>
+            <link rel="icon" href="/favicon.ico" sizes="any" />
+        </head>
+        <body className={(inter.className, "bg-black text-[#F5FFFA] antialiased")}>
+        {children}
+        </body>
+        </html>
+    );
 }
